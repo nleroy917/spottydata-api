c50d1b09aed8ff4303b0db3fd1b22681cf4f4874
Nathan LeRoy <NLeRoy917@gmail.com>
2020-02-01 23:03:27 -0500

    Updated CORS policy

--- api.py
+++ api.py
@@ -6,7 +6,9 @@ from lib.track_analysis import *
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 #import other necessary modules
 import json
