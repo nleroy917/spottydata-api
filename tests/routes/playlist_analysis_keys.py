dcab0f1ba6296a5e922457c792fcb8dcc9498192
Nathan LeRoy <NLeRoy917@gmail.com>
2020-02-07 15:18:28 -0500

    removed my access token + playlist id

--- tests/routes/playlist_analysis_keys.py
+++ tests/routes/playlist_analysis_keys.py
@@ -18,8 +18,4 @@ def test(BASE_URL,access_token,playlist_id):
 
 
 if __name__ == '__main__':
-
-	access_token = 'BQC8Ynw7HH2JCtT58q5zRyU7hicU4rIsU562YmwnfkRD7kadXqWS24n1WJUkUDMz62NUUZE3G0k3zz-fsxNmaXWwqvuSSBL0R-pWHXQf25IeyJGsPOm7ar6va-kRVJ4UngIYpcJApeHR6TbJzGWgKYvCXJCg3wVrbkOP3g'
-	playlist_id = '2GW9urbbhKa5TsDIthcdPV'
-
 	test(access_token,playlist_id)
